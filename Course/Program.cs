c81b6d24eb7e894901fe75944b3f24bcc340e25b
arthurvenicio <stuks.midia@gmail.com>
2022-01-29 02:05:56 -0300

    Exercise : Vectors

--- Course/Program.cs
+++ Course/Program.cs
@@ -8,43 +8,36 @@ namespace Course
         static void Main(string[] args)
         {
 
-            Account account;
+            Console.Write("How many rooms will be rented? ");
+            int rooms = int.Parse(Console.ReadLine());
 
-            Console.Write("Entre o número da conta: ");
-            int numberAccount = int.Parse(Console.ReadLine());
-            Console.Write("Entre o titular da conta: ");
-            string owner = Console.ReadLine();
-            Console.Write("Haverá depósitp inicial (s/n) ?");
-            char option = char.Parse(Console.ReadLine());
+            Rent[] rents = new Rent[10];
 
-            if (option == 's' || option == 'S')
+            for(int i = 0; i < rooms; i++)
             {
-                Console.Write("Entre o valor de depósito inicial: ");
-                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                account = new Account(numberAccount, owner, initialBalance);
+                int num = i + 1;
+                Console.WriteLine($"Rent #{num}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                rents[room] = new Rent(name, email, room);
+
+                Console.WriteLine();
             }
-            else
+
+            Console.WriteLine("Busy rooms:");
+
+            for(int i = 0;i < rents.Length; i++)
             {
-                 account = new Account(numberAccount, owner);
+                if (rents[i] != null)
+                {
+                    Console.WriteLine(i + ":" + rents[i].Name + "," + rents[i].Email);
+                }
             }
-
-
-            Console.WriteLine("Dados da conta:");
-            Console.WriteLine($"Conta {account.Number}, Titular: {account.Owner}, Saldo: $ {account.GetBalance().ToString("F2", CultureInfo.InvariantCulture)}");
-
-            Console.Write("Entre um valor para depósito: ");
-            double valueA = double.Parse(Console.ReadLine());
-            account.AddBalance(valueA);
-
-            Console.WriteLine("Dados da conta atualizados:");
-            Console.WriteLine($"Conta {account.Number}, Titular: {account.Owner}, Saldo: $ {account.GetBalance().ToString("F2", CultureInfo.InvariantCulture)}");
-
-            Console.Write("Entre um valor para saque: ");
-            double valueB = double.Parse(Console.ReadLine());
-            account.RemoveBalance(valueB );
-
-            Console.WriteLine("Dados da conta atualizados:");
-            Console.WriteLine($"Conta {account.Number}, Titular: {account.Owner}, Saldo: $ {account.GetBalance().ToString("F2", CultureInfo.InvariantCulture)}");
+            
         }
 
     }
---
+++ Course/Rent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class Rent
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Room { get; set; }
+
+        public Rent()
+        {
+           
+        }
+
+        public Rent(string name, string email, int room) {
+            this.Name = name;
+            this.Email = email;
+            this.Room = room;   
+        }
+
+    }
+}
